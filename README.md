c97abe386f638ef8e144302773d1a76f9dfce85d
Manuel <wtfjoke@livenet.ch>
2023-02-05 17:43:00 +0100

    🎉 Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # EmbyQNAPRepository
-QNAP Custom App Repostiroy for Emby Releases
+
+QNAP Custom App Repository for emby releases.
+
+## 🏋️ Motivation
+
+Since Emby is not yet listed in the official QNAP App Center ([source](https://emby.media/community/index.php?/topic/113735-qnap-emby-repository/#comment-1199816)), the only way to update Emby on a QNAP NAS is to download the newest release from [their page](https://emby.media/qnap-server.html) and upload it manually to your app center.
+
+There is the possibility to add 3rd party repositories in the app center, but emby doesn't provide any. Thats why this project was born.
+
+## 🔨 Run locally
+
+- Install [Groovy](https://groovy.apache.org/download.html)
+- Run script: `groovy createRepository.groovy`
---
+++ createRepository.groovy
@@ -0,0 +1,54 @@
+import groovy.xml.MarkupBuilder
+
+encoding = 'UTF-8'
+qnap = [
+    version: '4.3.4',
+    platforms: ['TS-NASX86': 'x86_64', 'TS-X28A': 'arm_64',
+                'TS-X32': 'arm_64', 'TS-X32U': 'arm_64',
+                'TS-XA28A': 'arm_64']
+]
+
+properties = loadProperties()
+rootDownloadUrl = properties.'release.download.url'
+appVersion = properties.'application.version'
+repositoryFile = new File('repository.xml')
+
+repositoryFile.withWriter(encoding) { writer ->
+    xml = new MarkupBuilder(writer)
+    xml.mkp.xmlDeclaration(version: '1.0', encoding: encoding)
+    xml.plugins {
+        cachechk(new Date().format('yyyyMMddhhmmss'))
+
+        item {
+            name('Emby')
+            internalName('emby')
+            category('Essentials')
+            type('Entertainment')
+            icon100('https://emby.media/community/uploads/inline/44692/560bd2026cb12_MB3_106_87.png')
+            icon80('https://emby.media/community/uploads/inline/44692/560bd22372364_MB3_85_70.png')
+            description(properties.'package.description')
+            developer('Emby LLC')
+            developerLink('https://emby.media/')
+            fwVersion(qnap.version)
+            version(properties.'application.version')
+            qnap.platforms.each { key, value ->
+                platform {
+                    platformID(key)
+                    location("${rootDownloadUrl}/${appVersion}/emby-server-qnap_${appVersion}_${value}.qpkg")
+                }
+            }
+            snapshot('https://emby.media/resources/Screenshot_2015-09-28-22-42-491.png')
+            tutorialLink('https://support.emby.media/support/solutions/articles/44001089172-home')
+            forumLink('https://emby.media/community/')
+        }
+    }
+}
+
+Properties loadProperties() {
+    properties = new Properties()
+    propertiesFile = new File('app.properties')
+    propertiesFile.withInputStream {
+        properties.load(it)
+    }
+    return properties
+}
